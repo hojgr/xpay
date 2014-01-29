15df3fa7b557727c1a73157b9a42999163954a0a
Michal Hojgr <michal.hojgr@gmail.com>
2014-01-30 00:27:31 +0100

    Fix compiler extension to actually work

--- src/Xpay/IMessageProcessor.php
+++ src/Xpay/IMessageProcessor.php
@@ -5,5 +5,6 @@ namespace Hicoria\Xpay;
 
 
 interface IMessageProcessor {
+    public function getRegexp();
     public function process($args);
 }
--- src/Xpay/XpayExtension.php
+++ src/Xpay/XpayExtension.php
@@ -5,6 +5,7 @@ namespace Hicoria\Xpay;
 
 
 use Nette\DI\CompilerExtension;
+use Nette\PhpGenerator\ClassType;
 
 class XpayExtension extends CompilerExtension {
     public function loadConfiguration()
@@ -12,7 +13,33 @@ class XpayExtension extends CompilerExtension {
         $config = $this->getConfig();
         $builder = $this->getContainerBuilder();
 
-        $this->compiler->parseServices($builder, $this->loadFromFile(__DIR__ . '/blog.neon'));
+        $builder->addDefinition($this->prefix('xpaySmsDispatcher'))
+            ->setClass('Hicoria\Xpay\XpaySmsDispatcher')
+            ->addTag('xpaySmsDispatcher');
+    }
 
+    /**
+     * @param ClassType $class
+     */
+    public function afterCompile(ClassType $class) {
+        /**
+         * @var IMessageProcessor $tagged
+         */
+
+        $initialize = $class->methods['initialize'];
+        $containerBuilder = $this->getContainerBuilder();
+
+        $dispatcher_name = key($containerBuilder->findByTag('xpaySmsDispatcher'));
+
+        foreach($containerBuilder->findByTag("xpaySmsProcessor") as $index => $value) {
+            $initialize->addBody(
+                '$this->getService(?)->register($this->getService(?)->getRegexp(), $this->getService(?));',
+                array(
+                    $dispatcher_name,
+                    $index,
+                    $index
+                )
+            );
+        }
     }
 } 
