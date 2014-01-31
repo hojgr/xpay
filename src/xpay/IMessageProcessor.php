5a5596acf5c702ee56b0ff7f6c23c71f28905718
Michal Hojgr <michal.hojgr@gmail.com>
2014-01-31 19:47:59 +0100

    fix interface issue

--- src/xpay/IMessageProcessor.php
+++ src/xpay/IMessageProcessor.php
@@ -6,5 +6,5 @@ namespace Hicoria\Xpay;
 
 interface IMessageProcessor {
     public function getRegexp();
-    public function process(array $args);
+    public function process(XpayMessageEntity $xpayMessageEntity, array $args);
 }
